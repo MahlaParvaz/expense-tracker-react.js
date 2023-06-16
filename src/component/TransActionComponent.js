1621383f73a0278699e5916cec5c39f01e856f96
Mahla <107788139+MahlaParvaz@users.noreply.github.com>
2023-06-16 11:47:41 +0330

    feat: added search

--- src/component/TransActionComponent.js
+++ src/component/TransActionComponent.js
@@ -1,23 +1,55 @@
+import { useEffect, useState } from 'react';
 const TransActionComponent = ({ transactions }) => {
+  const [searchItem, setSearchItem] = useState('');
+  const [filteredTns, setFilteredTns] = useState(transactions);
+
+  const filterTransactions = (search) => {
+    if (!search || search === '') {
+      setFilteredTns(transactions);
+      return;
+    }
+    const filtered = transactions.filter((t) =>
+      t.desc.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredTns(filtered);
+  };
+
+  const searchHandler = (e) => {
+    setSearchItem(e.target.value);
+    filterTransactions(e.target.value);
+  };
+  useEffect(() => {
+    filterTransactions(searchItem);
+  }, [transactions]);
+
+  if (!filterTransactions.length) return <p> Add Some Transaction</p>;
   return (
     <section>
-      {transactions &&
-        transactions.map((t) => (
-          <div
-            key={t.id}
-            className="transaction"
-            style={{
-              borderRight:
-                t.type === 'expense'
-                  ? ' 4px solid #16a34a '
-                  : ' 4px solid #dc2626',
-              backgroundColor: t.type === 'expense' ? '#f0fdf4' : '#fef2f2',
-            }}
-          >
-            <span>{t.desc}</span>
-            <span>{t.amount}</span>
-          </div>
-        ))}
+      <input
+        type="search "
+        placeholder="search ..."
+        value={searchItem}
+        onChange={searchHandler}
+        className="search"
+      />
+      {filteredTns.length
+        ? filteredTns.map((t) => (
+            <div
+              key={t.id}
+              className="transaction"
+              style={{
+                borderRight:
+                  t.type === 'expense'
+                    ? ' 4px solid #16a34a '
+                    : ' 4px solid #dc2626',
+                backgroundColor: t.type === 'expense' ? '#f0fdf4' : '#fef2f2',
+              }}
+            >
+              <span>{t.desc}</span>
+              <span>$ {t.amount} </span>
+            </div>
+          ))
+        : 'No Item Matchs !!'}
     </section>
   );
 };
